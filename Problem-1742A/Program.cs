395bee2dcf439eaa660a9bd47a17658ea3b7d3ec
Abdus Salam <salaam.mbstu@gmail.com>
2022-10-30 11:49:29 +0600

    A. Sum

---
+++ Problem-1742A/Program.cs
@@ -0,0 +1,13 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+
+    int a = int.Parse(parts[0]);
+    int b = int.Parse(parts[1]);
+    int c = int.Parse(parts[2]);
+
+    if(a + b == c || b + c == a || c + a == b) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
