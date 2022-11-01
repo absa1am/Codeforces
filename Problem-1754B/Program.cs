bd82aa3e5b0c80e1ef5579577edd9d7afea43f44
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-01 16:03:33 +0600

    B. Kevin and Permutation

---
+++ Problem-1754B/Program.cs
@@ -0,0 +1,13 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+for(int j = 0; j < t; j++)
+{
+    int n = int.Parse(Console.ReadLine());
+
+    int m = n / 2;
+    for(int i = 1; i <= m; i++)
+        Console.Write($"{i + m} {i} ");
+
+    if(n % 2 != 0) Console.WriteLine(n);
+    else Console.WriteLine();
+}
