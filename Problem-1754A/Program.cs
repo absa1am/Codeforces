a2838f819e34706f6814ea53979739bf1d096e81
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-01 15:09:55 +0600

    A. Technical Support

---
+++ Problem-1754A/Program.cs
@@ -0,0 +1,16 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int j = 0; j < t; j++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    string message = Console.ReadLine();
+
+    Stack<char> s = new Stack<char>();
+    for(int i = 0; i < n; i++)
+    {
+        if(message[i] == 'Q') s.Push(message[i]);
+        else s.TryPop(out char c);
+    }
+
+    Console.WriteLine(s.Count == 0? "Yes":"No");
+}
