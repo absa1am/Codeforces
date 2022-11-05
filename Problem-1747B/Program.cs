fdde2f43c634ff5018aff235ae884ef32ceff8a8
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-05 12:21:25 +0600

    B. BAN BAN

---
+++ Problem-1747B/Program.cs
@@ -0,0 +1,20 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    Dictionary<int, int> ops = new();
+    int n = int.Parse(Console.ReadLine());
+
+    for(int j = 1, k = 3 * n; j < k; j += 3)
+    {
+        if(j % 3 != 0)
+        {
+            ops.Add(j, k);
+            k = k - 3;
+        }
+    }
+
+    Console.WriteLine(ops.Count);
+    foreach (var op in ops)
+        Console.WriteLine($"{op.Key} {op.Value}");
+}
