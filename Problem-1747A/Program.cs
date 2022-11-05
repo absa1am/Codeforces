9b480502c7718824ad4eff2d295831fcf64db007
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-05 11:43:52 +0600

    A. Two Groups

---
+++ Problem-1747A/Program.cs
@@ -0,0 +1,17 @@
+﻿int t = int.Parse(Console.ReadLine());
+ 
+for(int j = 0; j < t; j++)
+{
+    long psum = 0, nsum = 0;
+    int n = int.Parse(Console.ReadLine());
+    string[] parts = Console.ReadLine().Split(' ');
+ 
+    for(int i = 0; i < n; i++)
+    {
+        if(int.Parse(parts[i]) < 0) nsum += int.Parse(parts[i]);
+        else psum += int.Parse(parts[i]);
+    }
+ 
+    nsum = -nsum;
+    Console.WriteLine(Math.Abs(psum - nsum));
+}
