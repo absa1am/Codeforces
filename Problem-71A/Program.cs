c91648f059dbaee35be1f63c5d873b478f383d33
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-08 12:51:50 +0600

    A. Way Too Long Words

---
+++ Problem-71A/Program.cs
@@ -0,0 +1,9 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+for(int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine();
+
+    if(s.Length <= 10) Console.WriteLine(s);
+    else Console.WriteLine($"{s[0]}{s.Length - 2}{s[^1]}");
+}
