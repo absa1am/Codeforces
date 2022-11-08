5bfcd1b5410f5a764bbee4a261ce469b7d59008d
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-08 13:03:40 +0600

    A. Team

---
+++ Problem-231A/Program.cs
@@ -0,0 +1,12 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+int problem = 0;
+for(int i = 0; i < n; i++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+
+    if(int.Parse(parts[0]) + int.Parse(parts[1]) + int.Parse(parts[2]) >= 2)
+        problem++;
+}
+
+Console.WriteLine(problem);
