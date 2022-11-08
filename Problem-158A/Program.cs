94893a7321586b6f4d5393e44ae104872270e631
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-08 13:50:54 +0600

    A. Next Round

---
+++ Problem-158A/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] parts;
+
+parts = Console.ReadLine().Split(' ');
+
+int n = int.Parse(parts[0]);
+int k = int.Parse(parts[1]);
+
+parts = Console.ReadLine().Split(' ');
+
+int kscore = int.Parse(parts[k - 1]);
+
+int ans = 0;
+for(int i = 0; i < n; i++)
+{
+    int x = int.Parse(parts[i]);
+    if(x > 0 && x >= kscore) ans++;
+}
+
+Console.WriteLine(ans);
