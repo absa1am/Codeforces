3d18afa198cc2362df8ad4fac5097440cb9dd83f
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-13 15:30:35 +0600

    A. Word Capitalization [Category: String, Features Used: ToUpper(), Substring()]

---
+++ Problem-281A/Program.cs
@@ -0,0 +1,6 @@
+﻿string word = Console.ReadLine();
+
+if(word.Length == 1) word = char.ToUpper(word[0]).ToString();
+else word = char.ToUpper(word[0]) + word.Substring(1);
+
+Console.WriteLine(word);
