fa47ac9b12861f61772d0f95ab05f1b064e9ceb4
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-13 15:03:24 +0600

    A. Beautiful Matrix [Category: Math, Features Used: Tuple, Math.Abs()]

---
+++ Problem-263A/Program.cs
@@ -0,0 +1,16 @@
+﻿int[,] mat = new int[5, 5];
+(int a, int b) location = (0, 0);
+
+for(int i = 0; i < 5; i++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+    
+    for(int j = 0; j < 5; j++)
+    {
+        mat[i, j] = int.Parse(parts[j]);
+        
+        if(mat[i, j] == 1) location = (i + 1, j + 1);
+    }
+}
+
+Console.WriteLine(Math.Abs(3 - location.a) + Math.Abs(3 - location.b));
