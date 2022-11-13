9c24c3e068a4d9da28748bd491e5f49ee2464457
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-13 14:39:07 +0600

    A. Bit++ [Category: String]

---
+++ Problem-282A/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int X = 0;
+for(int i = 0; i < n; i++)
+{
+    string cmd = Console.ReadLine();
+    
+    if(cmd == "++X" || cmd == "X++") X++;
+    else X--;
+}
+
+Console.WriteLine(X);
