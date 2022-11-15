cee16f29c2848a27ccb23448e262f27860726f18
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-15 22:41:20 +0600

    A. Nearly Lucky Number [Category: String, Math]

---
+++ Problem-110A/Program.cs
@@ -0,0 +1,20 @@
+﻿int digits = 0;
+bool isLucky = true;
+string n = Console.ReadLine();
+
+for(int i = 0; i < n.Length; i++)
+    if(n[i] == '4' || n[i] == '7') digits++;
+
+do
+{
+    if(digits % 10 != 4 && digits % 10 != 7)
+    {
+        isLucky = false;
+        break;
+    }
+
+    digits /= 10;
+}
+while(digits > 0);
+
+Console.WriteLine(isLucky? "YES":"NO");
