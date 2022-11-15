9c515b2e1ed0bfb2f4f374e4ed8a292e947dbe66
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-15 21:39:09 +0600

    A. Soldier and Bananas [Category: Math]

---
+++ Problem-546A/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] parts = Console.ReadLine().Split(' ');
+
+int.TryParse(parts[0], out int k);
+int.TryParse(parts[1], out int n);
+int.TryParse(parts[2], out int w);
+
+int cost = k * ((w * (w + 1)) / 2);
+
+if(cost <= n) Console.WriteLine(0);
+else Console.WriteLine(cost - n);
