5ef976e051de31b9496df92a25a32b30134a436e
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-15 13:39:16 +0600

    A. Boy or Girl [Category: String, Features Used: Dictionary]

---
+++ Problem-236A/Program.cs
@@ -0,0 +1,14 @@
+﻿string name = Console.ReadLine();
+Dictionary<char, byte> d = new();
+
+int count = 0;
+for(int i = 0; i < name.Length; i++)
+{
+    if(!d.ContainsKey(name[i]))
+    {
+        d.Add(name[i], 1);
+        count = count + 1;
+    }
+}
+
+Console.WriteLine(count % 2 == 0? "CHAT WITH HER!":"IGNORE HIM!");
