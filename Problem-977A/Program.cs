03b5c1c12b079b8e816a11ffb27be75b924c820e
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-15 22:17:35 +0600

    A. Wrong Subtraction [Category: Math]

---
+++ Problem-977A/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] parts = Console.ReadLine().Split(' ');
+
+int.TryParse(parts[0], out int n);
+int.TryParse(parts[1], out int k);
+
+for(int i = 0; i < k; i++)
+{
+    if(n % 10 != 0) n--;
+    else n /= 10;
+}
+
+Console.WriteLine(n);
