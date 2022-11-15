f6559a167b5daa4111e883bf2546c0c772049635
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-15 13:55:01 +0600

    A. Stones on the Table [Category: String, Counting]

---
+++ Problem-266A/Program.cs
@@ -0,0 +1,8 @@
+﻿byte n = byte.Parse(Console.ReadLine());
+string s = Console.ReadLine();
+
+byte count = 0;
+for(int i = 0; i <= n - 2; i++)
+    if(s[i] == s[i + 1]) count++;
+
+Console.WriteLine(count);
