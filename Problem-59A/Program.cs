d1f1874a0b72a6c2e33252f7e61a212a64228bc4
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-15 22:08:03 +0600

    A. Word [Category: String, Features Used: IsLower(), ToLower(), ToUpper()]

---
+++ Problem-59A/Program.cs
@@ -0,0 +1,11 @@
+﻿string word = Console.ReadLine();
+
+int count = 0;
+for(int i = 0; i < word.Length; i++)
+{
+    if(char.IsLower(word[i])) count--;
+    else count++;
+}
+
+if(count <= 0) Console.WriteLine(word.ToLower());
+else Console.WriteLine(word.ToUpper());
