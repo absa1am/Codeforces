aa27a10d4fe7fd0936d942ac086edde60cdd2e30
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-15 14:10:19 +0600

    A. Bear and Big Brother [Category: Math, Counting]

---
+++ Problem-791A/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] parts = Console.ReadLine().Split(' ');
+
+int.TryParse(parts[0], out int a);
+int.TryParse(parts[1], out int b);
+
+int year = 0;
+while(a <= b)
+{
+    a *= 3;
+    b *= 2;
+    year++;
+}
+
+Console.WriteLine(year);
