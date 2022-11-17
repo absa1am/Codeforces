501f630f7c8b09dcd2b901f8888f5c2819d564bb
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-17 14:15:16 +0600

    A. Tram [Category: Math, Features Used: Split(), Max()]

---
+++ Problem-116A/Program.cs
@@ -0,0 +1,12 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+int capacity = 0, ans = 0;
+for(int i = 0; i < n; i++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+
+    capacity = capacity + int.Parse(parts[1]) - int.Parse(parts[0]);
+    ans = Math.Max(capacity, ans);
+}
+
+Console.WriteLine(ans);
