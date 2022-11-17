66108a598de94431851773d92c9cf53f584f73fd
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-17 14:24:08 +0600

    A. Anton and Danik [Category: String, Math]

---
+++ Problem-734A/Program.cs
@@ -0,0 +1,10 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string s = Console.ReadLine();
+
+int anton = 0;
+for(int i = 0; i < n; i++)
+    if(s[i] == 'A') anton++;
+
+if(anton > n - anton) Console.WriteLine("Anton");
+else if(anton < n - anton) Console.WriteLine("Danik");
+else Console.WriteLine("Friendship");
