ab66b4f7d936eb3eb970aaf70bd31551cabffb5b
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-17 14:50:34 +0600

    B. Queue at the School [Category: String, Math]

---
+++ Problem-266B/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] parts = Console.ReadLine().Split(' ');
+char[] s = Console.ReadLine().ToArray();
+
+int.TryParse(parts[0], out int n);
+int.TryParse(parts[1], out int t);
+
+for(int j = 0; j < t; j++)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        if(s[i] == 'B' && s[i + 1] == 'G')
+        {
+            s[i] = 'G';
+            s[i + 1] = 'B';
+            i = i + 1;
+        }
+    }
+}
+
+Console.WriteLine(new string(s));
