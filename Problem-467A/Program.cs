ed232bc46b51e3b573dffbaf0b4ce6bf9bbf934a
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-19 18:16:23 +0600

    A. George and Accommodation [Category: Counting]

---
+++ Problem-467A/Program.cs
@@ -0,0 +1,12 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+int ans = 0;
+for(int i = 0; i < n; i++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+
+    if(int.Parse(parts[0]) + 2 <= int.Parse(parts[1]))
+        ans++;
+}
+
+Console.WriteLine(ans);
