7788989101644886f737f640857b04d09d1f6156
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-19 17:58:24 +0600

    A. Vanya and Fence [Category: Math]

---
+++ Problem-677A/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] parts;
+
+parts = Console.ReadLine().Split(' ');
+
+int.TryParse(parts[0], out int n);
+int.TryParse(parts[1], out int h);
+
+parts = Console.ReadLine().Split(' ');
+
+int ans = 0;
+for(int i = 0; i < n; i++)
+{
+    if(int.Parse(parts[i]) <= h) ans++;
+    else ans += 2;
+}
+
+Console.WriteLine(ans);
