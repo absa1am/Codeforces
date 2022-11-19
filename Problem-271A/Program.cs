5797b5debd684481f245a376092962e27c24e9ac
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-19 17:43:55 +0600

    A. Beautiful Year [Category: Math, Features Used: Dictionary]

---
+++ Problem-271A/Program.cs
@@ -0,0 +1,21 @@
+﻿int.TryParse(Console.ReadLine(), out int year);
+
+int y = year + 1;
+while(!IsDistinct(y)) y++;
+
+Console.WriteLine(y);
+
+bool IsDistinct(int y)
+{
+    Dictionary<int, bool> d = new();
+
+    do
+    {
+        if(d.ContainsKey(y % 10)) return false;
+        d.Add(y % 10, true);
+        y /= 10;
+    }
+    while(y > 0);
+
+    return true;
+}
