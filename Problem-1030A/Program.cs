fc21c88b9471ff0f9ab847ff0012ad46fe9a01c8
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-19 18:08:48 +0600

    A. In Search of an Easy Problem [Category: Counting]

---
+++ Problem-1030A/Program.cs
@@ -0,0 +1,14 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string[] parts = Console.ReadLine().Split(' ');
+
+bool easy = true;
+for(int i = 0; i < n; i++)
+{
+    if(parts[i] == "1")
+    {
+        easy = false;
+        break;
+    }
+}
+
+Console.WriteLine(easy? "EASY":"HARD");
