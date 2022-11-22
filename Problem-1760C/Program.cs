412f2c5cf872ae4bcba1abb6e177b7f3765688a2
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-22 18:37:15 +0600

    C. Advantage [Category: Sorting, Max finding]

---
+++ Problem-1760C/Program.cs
@@ -0,0 +1,29 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    string[] parts = Console.ReadLine().Split(' ');
+
+    int a = 0, b = 0;
+    for(int j = 0; j < n; j++)
+    {
+        int x = int.Parse(parts[j]);
+        if(x > b)
+        {
+            a = b;
+            b = int.Parse(parts[j]);
+        }
+        else if(x > a) a = x;
+    }
+
+    for(int j = 0; j < n; j++)
+    {
+        int x = int.Parse(parts[j]);
+
+        if(x != b) Console.Write(x - b + " ");
+        else Console.Write(x - a + " ");
+    }
+
+    Console.WriteLine();
+}
