8c6d7c163c26f60d8e3ce475bc14f2a10ad8e8bb
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-22 18:32:57 +0600

    A. Medium Number [Category: Math, Sorting, Features Used: Array.Sort()]

---
+++ Problem-1760A/Program.cs
@@ -0,0 +1,11 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+    int[] arr = {int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2])};
+
+    Array.Sort(arr);
+
+    Console.WriteLine(arr[1]);
+}
