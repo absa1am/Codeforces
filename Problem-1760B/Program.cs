ab932005e52a0b3dc21250b3bf0bee130ee90bb7
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-22 18:34:41 +0600

    B. Atilla's Favorite Problem [Category: Counting]

---
+++ Problem-1760B/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    string s = Console.ReadLine();
+
+    int ans = s[0] - 96;
+    for(int j = 1; j < s.Length; j++)
+        if(s[j] - 96 > ans) ans = s[j] - 96;
+
+    Console.WriteLine(ans);
+}
