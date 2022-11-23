19c6e3038493f61764396df23a8e54e67dd43f26
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-23 15:04:57 +0600

    A. Presents [Category: Array, Counting]

---
+++ Problem-136A/Program.cs
@@ -0,0 +1,11 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string[] friends = Console.ReadLine().Split(' ');
+byte[] answer = new byte[n + 1];
+
+for(byte i = 1; i <= n; i++)
+    answer[byte.Parse(friends[i - 1])] = i;
+
+for(byte i = 1; i <= n; i++)
+    Console.Write(answer[i] + " ");
+
+Console.WriteLine();
