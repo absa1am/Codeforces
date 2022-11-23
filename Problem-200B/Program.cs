6fa2ef53aba3f556076615d783d5b5dd5ca5b13f
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-23 16:20:18 +0600

    B. Drinks [Category: Math, Output Formatting]

---
+++ Problem-200B/Program.cs
@@ -0,0 +1,8 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string[] percent = Console.ReadLine().Split(' ');
+
+double volume = 0.0;
+for(int i = 0; i < n; i++)
+    volume += double.Parse(percent[i]);
+
+Console.WriteLine("{0:N12}", volume * (1.0 / n));
