c2da70659f85d36d706d7514e7491c8911442b72
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-23 15:26:43 +0600

    A. Magnets [Category: Counting]

---
+++ Problem-344A/Program.cs
@@ -0,0 +1,17 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+int previousMagnet = 0;
+int numberOfMagnetGroup = 0;
+
+for(int i = 0; i < n; i++)
+{
+    int.TryParse(Console.ReadLine(), out int magnet);
+
+    if(magnet != previousMagnet)
+    {
+        numberOfMagnetGroup++;
+        previousMagnet = magnet;
+    }
+}
+
+Console.WriteLine(numberOfMagnetGroup);
