c1334d56312951e2a08338644cd36eaa93025f8c
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-26 00:08:05 +0600

    A. SSeeeeiinngg DDoouubbllee

---
+++ Problem-1758A/Program.cs
@@ -0,0 +1,15 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string s = Console.ReadLine();
+    char[] palindrome = new char[s.Length * 2];
+
+    for(int j = 0, k = palindrome.Length - 1; j < s.Length; j++, k--)
+    {
+        palindrome[j] = s[j];
+        palindrome[k] = s[j];
+    }
+
+    Console.WriteLine(new string(palindrome));
+}
