64581e0fb6a8d4ed27bde1a82d316ffb27a1b727
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-26 00:08:45 +0600

    B. XOR = Average

---
+++ Problem-1758B/Program.cs
@@ -0,0 +1,20 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+
+    if(n % 2 == 0)
+    {
+        Console.Write("1");
+        for(int j = 1; j < n; j++)
+            Console.Write(" " + (n + 1));
+    }
+    else
+    {
+        for(int j = 0; j < n; j++)
+            Console.Write("1 ");
+    }
+
+    Console.WriteLine();
+}
