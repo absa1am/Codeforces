ba1032ac69a93cc2215fdcc9433994647285008c
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-27 14:44:08 +0600

    A. Doremy's Paint [Category: Math, Greedy]

---
+++ Problem-1764A/Program.cs
@@ -0,0 +1,9 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    Console.ReadLine();
+
+    Console.WriteLine($"1 {n}");
+}
