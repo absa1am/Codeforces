f9055bbf1f274c12648d266837c57a7faca0879a
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-27 15:47:38 +0600

    A. I Wanna Be the Guy [Category: Loop, Counting, Math, HashSet]

---
+++ Problem-469A/Program.cs
@@ -0,0 +1,15 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+string[] inputs;
+HashSet<string> uniqueLevels = new();
+
+inputs = Console.ReadLine().Split(' ');
+for(int i = 1; i < inputs.Length; i++)
+    uniqueLevels.Add(inputs[i]);
+
+inputs = Console.ReadLine().Split(' ');
+for(int i = 1; i < inputs.Length; i++)
+    uniqueLevels.Add(inputs[i]);
+
+if(uniqueLevels.Count == n) Console.WriteLine("I become the guy.");
+else Console.WriteLine("Oh, my keyboard!");
