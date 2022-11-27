776353b0b337b6033c5f2111fd4762e895d2ad03
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-27 15:07:40 +0600

    A. Hulk [Category: Loop, Modular Math]

---
+++ Problem-705A/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] feelings = {"I hate ", "I love "};
+
+int.TryParse(Console.ReadLine(), out int n);
+
+Console.Write(feelings[0]);
+for(int i = 1; i < n; i++)
+    Console.Write("{0} {1}", "that", feelings[i % 2]);
+
+Console.WriteLine("it");
