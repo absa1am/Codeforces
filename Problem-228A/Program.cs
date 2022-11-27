23ce2d2a301c5cebe380721e73a5aa56cc8fd5f7
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-27 15:23:10 +0600

    A. Is your horseshoe on the other hoof ? [Category: Counting, Math, Features Used: HashSet<>]

---
+++ Problem-228A/Program.cs
@@ -0,0 +1,7 @@
+﻿HashSet<string> uniqueColor = new();
+string[] inputs = Console.ReadLine().Split(' ');
+
+for(int i = 0; i < 4; i++)
+    uniqueColor.Add(inputs[i]);
+
+Console.WriteLine(4 - uniqueColor.Count);
