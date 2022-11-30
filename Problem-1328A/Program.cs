5199ac9204efbf0e391dd609de0e45ba605b48a8
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 00:13:26 +0600

    A. Divisibility Problem [Category: Math, Modular Arithmetic]

---
+++ Problem-1328A/Program.cs
@@ -0,0 +1,13 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string[] query = Console.ReadLine().Split(' ');
+
+    int.TryParse(query[0], out int a);
+    int.TryParse(query[1], out int b);
+
+    if(a % b == 0) Console.WriteLine(0);
+    else if(a < b) Console.WriteLine(b - a);
+    else Console.WriteLine((a / b) * b + b - a);
+}
