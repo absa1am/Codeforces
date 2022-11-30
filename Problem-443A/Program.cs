342591ea4c4a7405c39f7378882ae40e56fa0afd
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 00:39:48 +0600

    A. Anton and Letters [Category: String, Frequency Counting]

---
+++ Problem-443A/Program.cs
@@ -0,0 +1,17 @@
+﻿string set = Console.ReadLine();
+
+byte unique = 0;
+bool[] letters = new bool[26];
+for(int i = 0; i < set.Length; i++)
+{
+    if(set[i] >= 'a' && set[i] <= 'z')
+    {
+        if(!letters[set[i] - 97])
+        {
+            unique++;
+            letters[set[i] - 97] = true;
+        }
+    }
+}
+
+Console.WriteLine(unique);
