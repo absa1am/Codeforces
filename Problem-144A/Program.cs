7ee231fa3e3b00f256880be318ff084dcae74ed2
Abdus Salam <salaam.mbstu@gmail.com>
2022-11-30 09:45:47 +0600

    A. Arrival of the General [Category: Array]

---
+++ Problem-144A/Program.cs
@@ -0,0 +1,13 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string[] heights = Console.ReadLine().Split(' ');
+
+int maxIndex = 0, minIndex = 0;
+for(int i = 0; i < n; i++)
+{
+    if(int.Parse(heights[i]) > int.Parse(heights[maxIndex])) maxIndex = i;
+    if(int.Parse(heights[i]) <= int.Parse(heights[minIndex])) minIndex = i;
+}
+
+if(minIndex < maxIndex) minIndex++;
+
+Console.WriteLine(maxIndex - minIndex + n - 1);
