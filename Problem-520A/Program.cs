a73deb1be23f81223d4cb5cfbf57b2c6d15a0a99
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 00:28:36 +0600

    A. Pangram [Category: Frequency Counting, String]

---
+++ Problem-520A/Program.cs
@@ -0,0 +1,14 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string pangram = Console.ReadLine().ToLower();
+
+bool[] letters = new bool[26];
+
+int unique = 0;
+for(int i = 0; i < n; i++)
+{
+    if(!letters[pangram[i] - 97]) unique++;
+    letters[pangram[i] - 97] = true;
+}
+
+if(unique == 26) Console.WriteLine("YES");
+else Console.WriteLine("NO");
