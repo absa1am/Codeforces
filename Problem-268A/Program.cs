2ac2d5cedb2bcfc4bf6bb97237c405e72ee1fa0e
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 01:04:05 +0600

    A. Games [Category: Counting, Features Used: Array of Tuples]

---
+++ Problem-268A/Program.cs
@@ -0,0 +1,20 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+(int h, int g)[] teams = new (int h, int g)[n];
+
+for(int i = 0; i < n; i++)
+{
+    string[] team = Console.ReadLine().Split(' ');
+
+    teams[i].h = int.Parse(team[0]);
+    teams[i].g = int.Parse(team[1]);
+}
+
+int ans = 0;
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < n; j++)
+        if(teams[i].h == teams[j].g) ans++;
+}
+
+Console.WriteLine(ans);
