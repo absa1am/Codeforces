2fff6123a56673aa84acb87dc43f43b8f8e9b679
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 14:58:16 +0600

    A. Hit the Lottery [Category: Modular Arithmetic]

---
+++ Problem-996A/Program.cs
@@ -0,0 +1,12 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+int bills = 0;
+int[] denominations = {100, 20, 10, 5, 1};
+
+for(int i = 0; i < 5; i++)
+{
+    bills += n / denominations[i];
+    n = n - (n / denominations[i]) * denominations[i];
+}
+
+Console.WriteLine(bills);
