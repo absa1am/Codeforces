eb2d695c056efde13cb0da13c8bf910018e30548
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 16:13:42 +0600

    A. Candies and Two Sisters [Category: Math]

---
+++ Problem-1335A/Program.cs
@@ -0,0 +1,11 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+
+    if(n % 2 == 0) n--;
+
+    if(n <= 2) Console.WriteLine(0);
+    else Console.WriteLine(n / 2);
+}
