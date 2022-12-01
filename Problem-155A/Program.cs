ef42d7bbdb9dea108a4df03fae69ef445657d9a8
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-02 01:04:27 +0600

    A. I_love_%username% [Category: Array, Max Min Finding]

---
+++ Problem-155A/Program.cs
@@ -0,0 +1,23 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string[] points = Console.ReadLine().Split(' ');
+
+int min, max, ans = 0;
+
+max = min = int.Parse(points[0]);
+for(int i = 1; i < points.Length; i++)
+{
+    int point = int.Parse(points[i]);
+    
+    if(point > max)
+    {
+        ans++;
+        max = point;
+    }
+    else if(point < min)
+    {
+        ans++;
+        min = point;
+    }
+}
+
+Console.WriteLine(ans);
