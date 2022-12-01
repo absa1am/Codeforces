b34dd2f4526d5941f655dd0aada662c7060930d2
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 15:21:22 +0600

    A. Anton and Polyhedrons [Category: String, Features Used: Dictionary]

---
+++ Problem-785A/Program.cs
@@ -0,0 +1,15 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+Dictionary<string, int> polyhedron = new() {
+    ["Tetrahedron"] = 4,
+    ["Cube"] = 6,
+    ["Octahedron"] = 8,
+    ["Dodecahedron"] = 12,
+    ["Icosahedron"] = 20
+};
+
+int totalFaces = 0;
+for(int i = 0; i < n; i++)
+    totalFaces += polyhedron[Console.ReadLine()];
+
+Console.WriteLine(totalFaces);
