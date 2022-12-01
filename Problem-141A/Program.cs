9beb0ae0265c01af82624637f51e7ef2322a5c65
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 15:37:31 +0600

    A. Amusing Joke [Category: String, Sorting]

---
+++ Problem-141A/Program.cs
@@ -0,0 +1,11 @@
+﻿string guest = Console.ReadLine();
+string host = Console.ReadLine();
+char[] letters = Console.ReadLine().ToCharArray();
+char[] name = (guest + host).ToCharArray();
+
+Array.Sort(name);
+Array.Sort(letters);
+
+if(new string(name) == new string(letters))
+    Console.WriteLine("YES");
+else Console.WriteLine("NO");
