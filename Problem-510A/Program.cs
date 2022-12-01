d3d7e236f692a41327b85a189de7e71f7e9b4c17
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-01 23:41:54 +0600

    A. Fox And Snake [Category: Loop, if-else, Math]

---
+++ Problem-510A/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] line = Console.ReadLine().Split(' ');
+int n = int.Parse(line[0]);
+int m = int.Parse(line[1]);
+
+bool h = false;
+for(int i = 1; i <= n; i++)
+{
+    if(i % 2 != 0) Console.Write("#");
+    if(i % 2 == 0 && h) Console.Write("#");
+
+    for(int j = 2; j <= m; j++)
+    {
+        if(i % 2 != 0) Console.Write("#");
+        else Console.Write(".");
+    }
+
+    if(i % 2 == 0 && !h) Console.Write("#");
+    if(i % 2 == 0) h = h? false:true;
+
+    Console.WriteLine();
+}
