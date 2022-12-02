2914513daa3755eb84c1d55cf459aa08459b98e1
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-03 00:20:52 +0600

    A. Police Recruits [Category: Counting, if-else]

---
+++ Problem-427A/Program.cs
@@ -0,0 +1,15 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string[] line = Console.ReadLine().Split(' ');
+
+int untreated = 0;
+int policeMember = 0;
+for(int i = 0; i < n; i++)
+{
+    int x = int.Parse(line[i]);
+
+    if(x >= 0) policeMember += x;
+    else if(policeMember + x >= 0) policeMember--;
+    else untreated++;
+}
+
+Console.WriteLine(untreated);
