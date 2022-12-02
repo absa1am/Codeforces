f8c7b6db1524f28c526b2b78b6c412f1d6133dc5
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-03 00:50:51 +0600

    A. New Year and Hurry [Category: Loop, Math]

---
+++ Problem-750A/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] line = Console.ReadLine().Split(' ');
+
+int time = 240 - int.Parse(line[1]);
+int solved = 0, n = int.Parse(line[0]);
+
+for(int i = 1; i <= n; i++)
+{
+    if(5 * i <= time)
+    {
+        solved++;
+        time = time - (5 * i);
+    }
+    else break;
+}
+
+Console.WriteLine(solved);
