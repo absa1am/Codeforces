c967b77eecdcd8722747eff42bee972e9e9ea77c
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-03 22:40:58 +0600

    A. Vasya the Hipster [Category: Math, Features Used: Math.Min(), Math.Max()]

---
+++ Problem-581A/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] line = Console.ReadLine().Split(' ');
+
+int a = int.Parse(line[0]);
+int b = int.Parse(line[1]);
+
+Console.WriteLine("{0} {1}", Math.Min(a, b), (Math.Max(a, b) - Math.Min(a, b)) / 2);
