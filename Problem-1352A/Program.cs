81e3b76deaf6cd64e54b4258de04da5349750be4
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-07 01:15:07 +0600

    A. Sum of Round Numbers [Category: Math, Features Used: List<>]

---
+++ Problem-1352A/Program.cs
@@ -0,0 +1,27 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string n = Console.ReadLine();
+    List<string> ans = new List<string>();
+
+    for(int j = 0; j < n.Length; j++)
+    {
+        if(n[j] > '0')
+        {
+            string s = n[j].ToString();
+
+            for(int k = j + 1; k < n.Length; k++)
+                s += '0';
+
+            ans.Add(s);
+        }
+    }
+
+    Console.WriteLine(ans.Count);
+    
+    foreach (var s in ans)
+        Console.Write(s + " ");
+
+    Console.WriteLine();
+}
