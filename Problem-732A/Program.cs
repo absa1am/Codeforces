1cc0f6daff22c63e077b8de67e918e0056e16ea1
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-07 21:28:47 +0600

    A. Buy a Shovel [Category: Math, Loop]

---
+++ Problem-732A/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] line = Console.ReadLine().Split(' ');
+
+int.TryParse(line[0], out int k);
+int.TryParse(line[1], out int r);
+
+int ans = 1;
+while((k * ans - r) % 10 != 0 && (k * ans) % 10 != 0) ans++;
+
+Console.WriteLine(ans);
