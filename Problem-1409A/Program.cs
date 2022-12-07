85610b9159181fb787622e13aafcfb836ca26873
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-07 22:17:28 +0600

    A. Yet Another Two Integers Problem [Category: Math, Features Used: Math.Abs()]

---
+++ Problem-1409A/Program.cs
@@ -0,0 +1,8 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string[] input = Console.ReadLine().Split(' ');
+    int c = Math.Abs(int.Parse(input[0]) - int.Parse(input[1]));
+    Console.WriteLine(c / 10 + (c % 10 == 0? 0:1));
+}
