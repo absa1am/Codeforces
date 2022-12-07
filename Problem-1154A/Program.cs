2db1fc60f59631eba904e86f2011dacd8d766740
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-07 22:03:24 +0600

    A. Restoring Three Numbers [Category: Math, Sorting]

---
+++ Problem-1154A/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] input = Console.ReadLine().Split(' ');
+int[] numbers = {int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]), int.Parse(input[3])};
+
+Array.Sort(numbers);
+
+Console.WriteLine("{0} {1} {2}", numbers[3] - numbers[0], numbers[3] - numbers[1], numbers[3] - numbers[2]);
