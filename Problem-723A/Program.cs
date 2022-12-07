0a9de60e2ea4ec21c4704eb342fb39e41bb886e5
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-07 21:45:53 +0600

    A. The New Year: Meeting Friends [Category: Math, Sorting]

---
+++ Problem-723A/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] input = Console.ReadLine().Split(' ');
+int[] points = {int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2])};
+
+Array.Sort(points);
+
+Console.WriteLine(points[1] - points[0] + points[2] - points[1]);
