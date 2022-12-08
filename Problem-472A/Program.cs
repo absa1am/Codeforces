3a17641a4a31ed7810938f2656e2a6e856c6aad6
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-08 14:58:46 +0600

    A. Design Tutorial: Learn from Math [Category: Number Theory, Prime Number]

---
+++ Problem-472A/Program.cs
@@ -0,0 +1,21 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+if(n % 2 == 0) Console.WriteLine("4 {0}", n - 4);
+else {
+    for(int i = 4; i <= n; i++)
+    {
+        if(!IsPrime(i) && !IsPrime(n - i))
+        {
+            Console.WriteLine("{0} {1}", i, n - i);
+            break;
+        }
+    }
+}
+
+bool IsPrime(int n)
+{
+    for(int i = 2; i <= Math.Sqrt(n); i++)
+        if(n % i == 0) return false;
+    
+    return true;
+}
