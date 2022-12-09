3b9945df91833e49c078b4be49af86c257007612
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-09 15:28:47 +0600

    A. Holiday Of Equality [Category: Math, Array Manipulation]

---
+++ Problem-758A/Program.cs
@@ -0,0 +1,10 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+int[] citizens = Array.ConvertAll(Console.ReadLine().Split(' '), citizen => int.Parse(citizen));
+
+int maxWelfare = citizens.Max();
+int minBurleNeeded = 0;
+
+for(int i = 0; i < n; i++)
+    minBurleNeeded += maxWelfare - citizens[i];
+
+Console.WriteLine(minBurleNeeded);
