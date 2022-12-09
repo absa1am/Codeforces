42eb65a13ccd7f050924b5939d983d2c1bb3e385
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-09 14:16:09 +0600

    A. Choosing Teams [Category: Math, Sorting]

---
+++ Problem-432A/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] input;
+
+input = Console.ReadLine().Split(' ');
+
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+
+input = Console.ReadLine().Split(' ');
+
+int validTeam = 0;
+for(int i = 0; i < n; i++)
+{
+    if(k + int.Parse(input[i]) <= 5)
+        validTeam++;
+}
+
+Console.WriteLine(validTeam / 3);
