72c22e646dfa302f8041add4ef965dd451542c63
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-09 14:37:34 +0600

    A. Short Substrings [Category: String, Substring]

---
+++ Problem-1367A/Program.cs
@@ -0,0 +1,13 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string a, b = Console.ReadLine();
+
+    a = string.Empty;
+    for(int j = 0; j < b.Length; j += 2)
+        a += b[j];
+    
+    a += b[b.Length - 1];
+    Console.WriteLine(a);
+}
