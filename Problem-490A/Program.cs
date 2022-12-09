d27f31932424ae7c782140a35fed8181cbfaac34
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-09 21:47:52 +0600

    A. Team Olympiad [Category: Array Manipulation, Features Used: List<>, Array.ConvertAll<>, Math.Min()]

---
+++ Problem-490A/Program.cs
@@ -0,0 +1,19 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+int[] skills = Array.ConvertAll(Console.ReadLine().Split(' '), skill => int.Parse(skill));
+List<int> programmer = new();
+List<int> mathematician = new();
+List<int> physician = new(); 
+
+for(int i = 0; i < n; i++)
+{
+    if(skills[i] == 1) programmer.Add(i + 1);
+    else if(skills[i] == 2) mathematician.Add(i + 1);
+    else physician.Add(i + 1);
+}
+
+int team = Math.Min(Math.Min(programmer.Count, mathematician.Count), physician.Count);
+
+Console.WriteLine(team);
+
+for(int i = 0; i < team; i++)
+    Console.WriteLine("{0} {1} {2}", programmer[i], mathematician[i], physician[i]);
