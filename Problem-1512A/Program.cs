88c70f4c67f4e85de97749c45aa310c3423e0c1b
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-09 22:29:16 +0600

    A. Spy Detected! [Category: Array, if-else]

---
+++ Problem-1512A/Program.cs
@@ -0,0 +1,26 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    for(int j = 3; j <= n; j++)
+    {
+        if(numbers[j - 3] != numbers[j - 2] && numbers[j - 2] == numbers[j - 1])
+        {
+            Console.WriteLine(j - 2);
+            break;
+        }
+        else if(numbers[j - 3] != numbers[j - 2] && numbers[j - 3] == numbers[j - 1])
+        {
+            Console.WriteLine(j - 1);
+            break;
+        }
+        else if(numbers[j - 3] == numbers[j - 2] && numbers[j - 2] != numbers[j - 1])
+        {
+            Console.WriteLine(j);
+            break;
+        }
+    }
+}
