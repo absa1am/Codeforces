403321dbdd962610b2dfa4eb4bad4f4581a7478b
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-09 15:09:42 +0600

    B. Balanced Array [Category: Math]

---
+++ Problem-1343B/Program.cs
@@ -0,0 +1,29 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+
+    if(n % 2 == 1) Console.WriteLine("NO");
+    else {
+        if((n / 2) % 2 == 1) Console.WriteLine("NO");
+        else {
+            Console.WriteLine("YES");
+            
+            int sum = 0;
+            for(int j = 2; j <= n; j += 2)
+            {
+                sum += j;
+                Console.Write(j + " ");
+            }
+            
+            for(int j = 1; j <= n - 2; j += 2)
+            {
+                sum -= j;
+                Console.Write(j + " ");
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
