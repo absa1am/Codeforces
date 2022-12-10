d35a70c33f56824457285ea1ab6f3ab2f5ed7250
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-10 16:53:30 +0600

    A. Mishka and Game [Category: Counting]

---
+++ Problem-703A/Program.cs
@@ -0,0 +1,14 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+int m = 0, c = 0;
+for(int i = 0; i < n; i++)
+{
+    string[] score = Console.ReadLine().Split(' ');
+
+    if(int.Parse(score[0]) > int.Parse(score[1])) m++;
+    else if(int.Parse(score[1]) > int.Parse(score[0])) c++;
+}
+
+if(m == c) Console.WriteLine("Friendship is magic!^^");
+else if(m > c) Console.WriteLine("Mishka");
+else Console.WriteLine("Chris");
