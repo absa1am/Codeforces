550f4b5f063dee4e5accf8526e411618fe215250
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-10 18:51:10 +0600

    B. Borze [Category: String, if-else]

---
+++ Problem-32B/Program.cs
@@ -0,0 +1,17 @@
+﻿string brozeCode = Console.ReadLine();
+
+for(int i = 0; i < brozeCode.Length; i++)
+{
+    if(brozeCode[i] == '.') Console.Write("0");
+    else if(brozeCode[i + 1] == '.')
+    {
+        i++;
+        Console.Write("1");
+    }
+    else {
+        i++;
+        Console.Write("2");
+    }
+}
+
+Console.WriteLine();
