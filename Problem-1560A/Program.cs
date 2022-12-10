c9eaf17459cfd0d60154207e9d856352e19d6e51
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-10 16:34:20 +0600

    A. Dislike of Threes [Category: Counting, if-else]

---
+++ Problem-1560A/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> numbers = new();
+
+int k = 0;
+for(int i = 1; ; i++)
+{
+    if(i % 3 != 0 && i % 10 != 3)
+    {
+        k++;
+        numbers.Add(i);
+        if(k == 1000) break;
+    }
+}
+
+int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out k);
+
+    Console.WriteLine(numbers[k - 1]);
+}
