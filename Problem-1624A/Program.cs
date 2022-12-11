9c4f71482a53917c8159d6e650467c4fdbe99a20
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-11 16:23:31 +0600

    A. Plus One on the Subset [Category: Max, Min]

---
+++ Problem-1624A/Program.cs
@@ -0,0 +1,9 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    Console.WriteLine(numbers.Max() - numbers.Min());
+}
