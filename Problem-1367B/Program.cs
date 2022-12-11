8094cf585c2bc1ea0f90d6a3a700b6cfc0c23246
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-11 14:41:31 +0600

    B. Even Array [Category: Odd Even Counting, if-else]

---
+++ Problem-1367B/Program.cs
@@ -0,0 +1,17 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), a => int.Parse(a));
+
+    int even = 0, odd = 0;
+    for(int j = 0; j < n; j++)
+    {
+        if(j % 2 == 0 && numbers[j] % 2 != 0) odd++;
+        else if(j % 2 != 0 && numbers[j] % 2 == 0) even++;
+    }
+
+    if(odd != even) Console.WriteLine(-1);
+    else Console.WriteLine(odd);
+}
