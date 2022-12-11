62c3f0039486c4737a1514d88c9ac088271f7059
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-11 16:14:21 +0600

    B. Two Arrays And Swaps [Category: Sorting, Array Manipulation]

---
+++ Problem-1353B/Program.cs
@@ -0,0 +1,20 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int[] nk = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+    int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+    int[] b = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    Array.Sort(a);
+    Array.Sort(b, (x, y) => y - x);
+
+    int sum = 0;
+    for(int j = 0; j < nk[0]; j++)
+    {
+        if(j < nk[1] && b[j] > a[j]) sum += b[j];
+        else sum += a[j];
+    }
+
+    Console.WriteLine(sum);
+}
