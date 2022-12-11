a85e48b92daa5ce9bf08849e7c8859ba8279c086
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-11 15:20:43 +0600

    A. Do Not Be Distracted! [Category: String, Loop, Dictionary]

---
+++ Problem-1520A/Program.cs
@@ -0,0 +1,26 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    string s = Console.ReadLine();
+
+    bool notSuspicious = true;
+    Dictionary<char, bool> map = new();
+
+    map.Add(s[0], true);
+    for(int j = 1; j < n; j++)
+    {
+        if(s[j - 1] != s[j])
+        {
+            if(!map.ContainsKey(s[j])) map.Add(s[j], true);
+            else {
+                notSuspicious = false;
+                break;
+            }
+        }
+    }
+
+    if(notSuspicious) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
