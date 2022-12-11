dcba16888ca1ed72cb1d66b31d1a4b72902ff50f
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-12 01:21:21 +0600

    A. Hossam and Combinatorics [Category: Math]

---
+++ Problem-1771A/Program.cs
@@ -0,0 +1,20 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+    
+    int numberOfMax = 0;
+    int numberOfMin = 0;
+    int max = numbers.Max();
+    int min = numbers.Min();
+    for(int j = 0; j < n; j++)
+    {
+        if(numbers[j] == min) numberOfMin++;
+        if(numbers[j] == max) numberOfMax++;
+    }
+
+    if(min == max) Console.WriteLine((long) n * (long) (n - 1));
+    else Console.WriteLine((long) numberOfMax * (long) numberOfMin * 2);
+}
