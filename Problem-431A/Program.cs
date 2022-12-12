913d9cb6e1104888c5f21208dad10dd7bd9d1dfb
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-12 15:29:46 +0600

    A. Black Square [Category: String, Math]

---
+++ Problem-431A/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] strips = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+string s = Console.ReadLine();
+
+int ans = 0;
+for(int i = 0; i < s.Length; i++)
+    ans += strips[s[i] - 48 - 1];
+
+Console.WriteLine(ans);
