b6df59865673f37d73b7c2260dc4226632c56b61
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-12 15:13:49 +0600

    A. Boring Apartments [Category: Math, String]

---
+++ Problem-1433A/Program.cs
@@ -0,0 +1,9 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string x = Console.ReadLine();
+
+    int ans = ((x[0] - 48) - 1) * 10 + ((x.Length * (x.Length + 1)) / 2);
+    Console.WriteLine(ans);
+}
