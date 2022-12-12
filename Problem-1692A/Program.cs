b176150ebfebae38d920f5afa73a86ab485d1db4
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-12 16:49:03 +0600

    A. Marathon [Category: Counting, if-else]

---
+++ Problem-1692A/Program.cs
@@ -0,0 +1,13 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int[] distances = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    int ans = 0;
+    for(int j = 1; j < 4; j++)
+        if(distances[j] > distances[0]) ans++;
+
+    Console.WriteLine(ans);
+}
+
