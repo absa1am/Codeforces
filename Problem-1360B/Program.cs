53a90ca26be44697341cc40b6ec51cf4a4e16f4e
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-12 16:37:23 +0600

    B. Honest Coach [Category: Sorting, Features Used: Array.Sort()]

---
+++ Problem-1360B/Program.cs
@@ -0,0 +1,18 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] strengths = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    Array.Sort(strengths);
+
+    int min = int.MaxValue;
+    for(int j = 1; j < n; j++)
+    {
+        int s = strengths[j] - strengths[j - 1];
+        if(s < min) min = s;
+    }
+
+    Console.WriteLine(min);
+}
