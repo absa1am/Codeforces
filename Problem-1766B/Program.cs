99bca6298c948bed3a43025879f2d6c7a0e0c1da
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-13 16:53:34 +0600

    B. Notepad# [Category: Map, Sub-string]

---
+++ Problem-1766B/Program.cs
@@ -0,0 +1,24 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    string s = Console.ReadLine();
+
+    Dictionary<string, int> d = new();
+
+    bool ans = false;
+    for(int j = 1; j < n; j++)
+    {
+        string x = s[j - 1].ToString() + s[j].ToString();
+        
+        if(!d.ContainsKey(x)) d.Add(x, j);
+        else if(d[x] < j - 1) {
+            ans = true;
+            break;
+        }
+    }
+
+    if(ans) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
