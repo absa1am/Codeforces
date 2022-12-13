00bcec2e7accf6db96630fb354018acb12f38358
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-13 16:44:17 +0600

    B. Fair Division [Category: Math, Counting]

---
+++ Problem-1472B/Program.cs
@@ -0,0 +1,18 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] weights = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    int one = 0, two = 0;
+    for(int j = 0; j < n; j++)
+    {
+        if(weights[j] == 1) one++;
+        else two++;
+    }
+
+    if(two % 2 != 0) one = one - 2;
+    if(one >= 0 && one % 2 == 0) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
