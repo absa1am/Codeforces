c778e8702d9724dcc1254aa260f6bf6209825b22
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-13 16:49:21 +0600

    A. Extremely Round [Category: Math]

---
+++ Problem-1766A/Program.cs
@@ -0,0 +1,8 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+int[] denominatior = {1, 10, 100, 1000, 10000, 100000};
+
+for(int i = 0; i < t; i++)
+{
+    string n = Console.ReadLine();
+    Console.WriteLine((int.Parse(n) / denominatior[n.Length - 1]) + (9 * (n.Length - 1)));
+}
