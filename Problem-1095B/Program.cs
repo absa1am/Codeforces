2a48bfcc8721e0d9cbfbd8b853ec6b5626ecb625
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-15 17:15:52 +0600

    B. Array Stabilization [Category: Math, Max Min]

---
+++ Problem-1095B/Program.cs
@@ -0,0 +1,9 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+int[] elements = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+Array.Sort(elements);
+
+if(n == 2) Console.WriteLine(0);
+else if(elements[n - 1] - elements[1] < elements[n - 2] - elements[0])
+    Console.WriteLine(elements[n - 1] - elements[1]);
+else Console.WriteLine(elements[n - 2] - elements[0]);
