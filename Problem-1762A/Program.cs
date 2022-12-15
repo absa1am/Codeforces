1be81dba01c3cb9f4013d92723280a8adf7a8920
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-16 00:15:05 +0600

    A. Divide and Conquer [Category: Math, Greedy]

---
+++ Problem-1762A/Program.cs
@@ -0,0 +1,40 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    int odd = 0, minOdd, minEven;
+
+    minOdd = minEven = int.MaxValue;
+    
+    for(int j = 0; j < n; j++)
+    {
+        int count = 0, x = numbers[j];
+
+        if(x % 2 != 0)
+        {
+            odd++;
+            while(x % 2 != 0)
+            {
+                x /= 2;
+                count++;
+            }
+
+            if(count < minOdd) minOdd = count;
+        }
+        else {
+            while(x % 2 == 0)
+            {
+                x /= 2;
+                count++;
+            }
+
+            if(count < minEven) minEven = count;
+        }
+    }
+    
+    if(odd % 2 == 0) Console.WriteLine(0);
+    else Console.WriteLine(Math.Min(minEven, minOdd));
+}
