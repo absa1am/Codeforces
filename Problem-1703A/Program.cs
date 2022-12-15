7c830feefff6b126a3523ce899d658aa61419f00
Abdus Salam <salaam.mbstu@gmail.com>
2022-12-16 00:22:27 +0600

    A. YES or YES? [Category: String Comparison]

---
+++ Problem-1703A/Program.cs
@@ -0,0 +1,9 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string s = Console.ReadLine();
+
+    if(s.ToLower() == "yes") Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
