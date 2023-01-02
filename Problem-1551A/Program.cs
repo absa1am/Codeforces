ab85808a874af48e0c7c3f4a82bd3a059051dbc6
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-02 15:11:36 +0600

    A. Polycarp and Coins [Category: Math, Features Used: Math.Floor(), Math.Ceiling()]

---
+++ Problem-1551A/Program.cs
@@ -0,0 +1,12 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+
+    int p = (int) Math.Floor((decimal) n / 3);
+    int q = (int) Math.Ceiling((decimal) n / 3);
+
+    if(p + 2 * q == n) Console.WriteLine("{0} {1}", p, q);
+    else Console.WriteLine("{0} {1}", q, p);
+}
