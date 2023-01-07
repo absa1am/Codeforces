73f98a132900225ee0182ea31f5577361d2245bb
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-07 16:25:55 +0600

    B. Gifts Fixing [Category: Math, Min Max Finding]

---
+++ Problem-1399B/Program.cs
@@ -0,0 +1,20 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+
+    int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+    int[] b = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    long ans = 0;
+    int minA = a.Min();
+    int minB = b.Min();
+    for(int j = 0; j < n; j++)
+    {
+        if(a[j] == minA || b[j] == minB) ans += (a[j] - minA) + (b[j] - minB);
+        else ans += Math.Max(a[j] - minA, b[j] - minB);
+    }
+
+    Console.WriteLine(ans);
+}
