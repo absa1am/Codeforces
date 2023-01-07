f75593b91f89cc74fe0e65f730accf0d1eea20e7
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-07 16:47:49 +0600

    A. Division ? [Category: if-else]

---
+++ Problem-1669A/Program.cs
@@ -0,0 +1,14 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string division = "Division ";
+    int.TryParse(Console.ReadLine(), out int rating);
+
+    if(rating <= 1399) division += "4";
+    else if(rating >= 1400 && rating <= 1599) division += "3";
+    else if(rating >= 1600 && rating <= 1899) division += "2";
+    else division += "1";
+
+    Console.WriteLine(division);
+}
