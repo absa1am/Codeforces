4e83af8d0b0b322605f7b792064aa388f298e32f
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-07 16:34:26 +0600

    A. Lucky ? [Category: Math, String]

---
+++ Problem-1676A/Program.cs
@@ -0,0 +1,18 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string s = Console.ReadLine();
+
+    if(Sum(s, 0, 2) == Sum(s, 3, 5)) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
+
+int Sum(string number, int startIndex, int endIndex)
+{
+    int sum = 0;
+    for(int i = startIndex; i <= endIndex; i++)
+        sum += number[i] - '0';
+    
+    return sum;
+}
