e5f920a99173a8ace9d1b80b135b674e16249167
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-08 21:44:35 +0600

    A. Fair Playoff [Category: Min Max]

---
+++ Problem-1535A/Program.cs
@@ -0,0 +1,10 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    if(Math.Max(arr[0], arr[1]) < Math.Min(arr[2], arr[3]) || Math.Max(arr[2], arr[3]) < Math.Min(arr[0], arr[1]))
+        Console.WriteLine("NO");
+    else Console.WriteLine("YES");
+}
