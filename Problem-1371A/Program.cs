a9589d2ac8a7c0ade3360dde1630e04236888401
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-08 17:26:18 +0600

    A. Magical Sticks [Category: Math, Odd Even]

---
+++ Problem-1371A/Program.cs
@@ -0,0 +1,9 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+
+    if(n % 2 == 0) Console.WriteLine(n / 2);
+    else Console.WriteLine((n + 1) / 2);
+}
