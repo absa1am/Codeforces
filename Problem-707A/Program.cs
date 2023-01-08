6d819e6147c628f09f8d827837cb8458aa8c8969
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-08 17:42:08 +0600

    A. Brain's Photos [Category: if-else on matrix]

---
+++ Problem-707A/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] dimension = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+bool isColoredPicture = false;
+for(int i = 0; i < dimension[0]; i++)
+{
+    string line = Console.ReadLine();
+
+    for(int j = 0; j < line.Length; j++)
+    {
+        if(line[j] == 'C' || line[j] == 'M' || line[j] == 'Y')
+        {
+            isColoredPicture = true;
+            break;
+        }
+    }
+}
+
+if(isColoredPicture) Console.WriteLine("#Color");
+else Console.WriteLine("#Black&White");
