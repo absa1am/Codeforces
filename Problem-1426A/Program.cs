2a0f87b43944ff8b697a5a4f74fd7b495301c84b
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-08 23:27:29 +0600

    A. Floor Number [Category: Loop, Math]

---
+++ Problem-1426A/Program.cs
@@ -0,0 +1,18 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+
+    int n = int.Parse(parts[0]);
+    int x = int.Parse(parts[1]);
+
+    int sum = 2, count = 1;
+    while(sum < n)
+    {
+        count++;
+        sum += x;
+    }
+
+    Console.WriteLine(count);
+}
