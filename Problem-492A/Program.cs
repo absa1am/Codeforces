b2a22d2f65ee35bbabd00833eda4f754df0d447f
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-08 18:04:31 +0600

    A. Vanya and Cubes [Category: Math, Use of n(n + 1)/2 Formula]

---
+++ Problem-492A/Program.cs
@@ -0,0 +1,11 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+
+int sum = 1, count = 0;
+
+while(sum <= n)
+{
+    count++;
+    sum += ((count + 1) * (count + 2)) / 2;
+}
+
+Console.WriteLine(count);
