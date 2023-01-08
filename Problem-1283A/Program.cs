aaa725797009fe5fc88dc9438f0fcf231482f29f
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-08 22:50:55 +0600

    A. Minutes Before the New Year [Category: Simple Math]

---
+++ Problem-1283A/Program.cs
@@ -0,0 +1,8 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int[] time = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    Console.WriteLine((23 - time[0]) * 60 + (60 - time[1]));
+}
