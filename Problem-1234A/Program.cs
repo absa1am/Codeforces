2f7f022dc309db32f5e1991163ca24d9269edc05
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-10 03:46:54 +0600

    A. Equalize Prices Again [Category: Math]

---
+++ Problem-1234A/Program.cs
@@ -0,0 +1,20 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] goods = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    int sum = goods.Sum();
+    int min = goods.Min();
+    int max = goods.Max();
+
+    for(int j = min;  j <= max; j++)
+    {
+        if(n * j >= sum)
+        {
+            Console.WriteLine(j);
+            break;
+        }
+    }
+}
