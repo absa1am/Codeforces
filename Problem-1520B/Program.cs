3f7d1040f129c76d8717e80cf42d90978175d990
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-10 02:40:57 +0600

    B. Ordinary Numbers [Category: String, Math]

---
+++ Problem-1520B/Program.cs
@@ -0,0 +1,11 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string n = Console.ReadLine();
+
+    int ans = 9 * (n.Length - 1) + (n[0] - '0' - 1);
+    
+    if(string.Compare(new string(n[0], n.Length), n) <= 0) Console.WriteLine(ans + 1);
+    else Console.WriteLine(ans);
+}
