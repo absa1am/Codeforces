d34f0493b36084d89604e8b286d070b0d91c1c77
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-10 03:26:40 +0600

    A. Cards [Category: Character Counting]

---
+++ Problem-1220A/Program.cs
@@ -0,0 +1,26 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string s = Console.ReadLine();
+
+int numberOfN = 0, numberOfZ = 0;
+for(int i = 0; i < n; i++)
+{
+    if(s[i] == 'n') numberOfN++;
+    else if(s[i] == 'z') numberOfZ++;
+}
+
+int len = numberOfN + numberOfZ;
+for(int i = 0; i < len; i++)
+{
+    if(numberOfN > 0)
+    {
+        Console.Write("1 ");
+        numberOfN--;
+    }
+    else
+    {
+        Console.Write("0 ");
+        numberOfZ--;
+    }
+}
+
+Console.WriteLine();
