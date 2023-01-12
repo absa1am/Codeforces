6594bc782f9a508f7f79024e4a8f318b2db8c5cc
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-13 00:49:42 +0600

    A. Odd Set [Category: Odd Even Counting]

---
+++ Problem-1542A/Program.cs
@@ -0,0 +1,19 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    int numberOfOdd, numberOfEven;
+
+    numberOfOdd = numberOfEven = 0;
+    for(int j = 0; j < 2 * n; j++)
+    {
+        if(numbers[j] % 2 == 0) numberOfEven++;
+        else numberOfOdd++;
+    }
+
+    if(numberOfOdd != numberOfEven) Console.WriteLine("NO");
+    else Console.WriteLine("YES");
+}
