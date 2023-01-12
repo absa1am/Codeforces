691cb67af741758e0d33b0454d7be1ff65f29ba2
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-13 00:06:35 +0600

    A. Phoenix and Balance [Category: Math, Loop]

---
+++ Problem-1348A/Program.cs
@@ -0,0 +1,23 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+
+    int h = n / 2;
+    long[] arr = new long[n];
+
+    arr[0] = 2;
+    for(int j = 1; j < n; j++)
+        arr[j] = arr[j - 1] * 2;
+    
+    long pileA = arr[n - 1];
+    for(int j = 0; j < h - 1; j++)
+        pileA += arr[j];
+
+    long pileB = 0;
+    for(int j = n - 2; j > n - 2 - h; j--)
+        pileB += arr[j];
+    
+    Console.WriteLine(Math.Abs(pileA - pileB));
+}
