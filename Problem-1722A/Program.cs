85520a4345c6f815c88df34c03eadeb200ef3dde
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-12 21:20:58 +0600

    A. Spell Check [Category: String, Sorting]

---
+++ Problem-1722A/Program.cs
@@ -0,0 +1,12 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    char[] name = Console.ReadLine().ToCharArray();
+
+    Array.Sort(name);
+    
+    if(new string(name) == "Timru") Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
