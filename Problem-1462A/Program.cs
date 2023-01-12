42d738a437ea7a8b4f5a4afe845fd17391a90e09
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-13 00:38:12 +0600

    A. Favorite Sequence [Category: Loop]

---
+++ Problem-1462A/Program.cs
@@ -0,0 +1,15 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] num = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    for(int j = 0, k = n - 1; j <= k; j++, k--)
+    {
+        Console.Write("{0} ", num[j]);
+        if(j != k) Console.Write("{0} ", num[k]);
+    }
+
+    Console.WriteLine();
+}
