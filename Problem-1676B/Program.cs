8dac58a5e301f09b0f2063a3c4c579ca06c8a6b3
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-13 20:12:57 +0600

    B. Equal Candies [Category: Math, Min Finding, Greedy]

---
+++ Problem-1676B/Program.cs
@@ -0,0 +1,14 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] candies = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    long ans = 0;
+    int min = candies.Min();
+    for(int j = 0; j < n; j++)
+        ans += candies[j] - min;
+    
+    Console.WriteLine(ans);
+}
