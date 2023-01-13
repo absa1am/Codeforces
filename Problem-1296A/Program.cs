70299059bb645a58706347cf111ccfee2fce56cd
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-13 22:33:38 +0600

    A. Array with Odd Sum [Category: Odd Even Check, Math]

---
+++ Problem-1296A/Program.cs
@@ -0,0 +1,20 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] elements = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    int odd, even, sum;
+
+    odd = even = sum = 0;
+    for(int j = 0; j < n; j++)
+    {
+        sum += elements[j];
+        if(elements[j] % 2 == 0) even++;
+        else odd++;
+    }
+
+    if(sum % 2 != 0 || (odd > 0 && even > 0)) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
