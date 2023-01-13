6b47f4ef2e0d0c15b6ad4f58cb13c7b39802af4c
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-13 15:49:43 +0600

    A. Add Odd or Subtract Even [Category: Odd Even Test]

---
+++ Problem-1311A/Program.cs
@@ -0,0 +1,21 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int[] ab = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    int ans = 0;
+
+    if(ab[0] > ab[1])
+    {
+        if((ab[0] % 2 == 0 && ab[1] % 2 != 0) || (ab[0] % 2 != 0 && ab[1] % 2 == 0)) ans = 2;
+        else ans = 1;
+    }
+    else if(ab[0] < ab[1])
+    {
+        if((ab[0] % 2 == 0 && ab[1] % 2 == 0) || (ab[0] % 2 != 0 && ab[1] % 2 != 0)) ans = 2;
+        else ans = 1;
+    }
+
+    Console.WriteLine(ans);
+}
