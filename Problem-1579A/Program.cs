7e1ffaebe4319b47b2e4aa5287d7134314a408cf
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-13 23:39:04 +0600

    A. Casimir's String Solitaire [Category: String, if-else, Character Counting]

---
+++ Problem-1579A/Program.cs
@@ -0,0 +1,41 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string s = Console.ReadLine();
+
+    int a, b, c;
+
+    a = b = c = 0;
+    foreach(char ch in s)
+    {
+        if(ch == 'A') a++;
+        else if(ch == 'B') b++;
+        else c++;
+    }
+
+    if(a > b)
+    {
+        a = a - b;
+        b = 0;
+    }
+    else
+    {
+        b = b - a;
+        a = 0;
+    }
+
+    if(c > b)
+    {
+        c = c - b;
+        b = 0;
+    }
+    else
+    {
+        b = b - c;
+        c = 0;
+    }
+
+    if(a == 0 && b == 0 && c == 0) Console.WriteLine("YES");
+    else Console.WriteLine("NO");
+}
