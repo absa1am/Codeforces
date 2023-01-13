e5634d60fa55172771abb653ddc5287e22d0c9ad
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-13 16:19:07 +0600

    A. Park Lighting [Category: Math, Odd Even Check]

---
+++ Problem-1358A/Program.cs
@@ -0,0 +1,9 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int[] nm = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    if((nm[0] * nm[1]) % 2 == 0) Console.WriteLine((nm[0] * nm[1]) / 2);
+    else Console.WriteLine(((nm[0] * nm[1]) / 2) + 1);
+}
