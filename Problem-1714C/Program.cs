c2db9497ec1467aa64dcc55a738a7785d4df8d2a
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-15 22:47:35 +0600

    C. Minimum Varied Number [Category: String, Math]

---
+++ Problem-1714C/Program.cs
@@ -0,0 +1,22 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int s);
+
+    int k = 9, n = 0;
+    string ans = string.Empty;
+
+    while(k > 0)
+    {
+        if(n + k <= s)
+        {
+            n += k;
+            ans = k.ToString() + ans;
+        }
+        
+        k--;
+    }
+
+    Console.WriteLine(ans);
+}
