67ff7c4774c9556dc92fd781fe1c09b9edf7b3d2
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-17 21:21:47 +0600

    A. Gennady and a Card Game [Category: Character Comparison]

---
+++ Problem-1097A/Program.cs
@@ -0,0 +1,15 @@
+﻿string tableCard = Console.ReadLine();
+string[] handCard = Console.ReadLine().Split(' ');
+
+bool canPlay = false;
+for(int i = 0; i < handCard.Length; i++)
+{
+    if (tableCard[0] == handCard[i][0] || tableCard[1] == handCard[i][1])
+    {
+        canPlay = true;
+        break;
+    }
+}
+
+if(canPlay) Console.WriteLine("YES");
+else Console.WriteLine("NO");
