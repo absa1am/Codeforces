df712cf2f4353b05abea190e1ca8f82ea19370d1
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-17 16:10:40 +0600

    B. ICPC Balloons [Category: Array or Dictionary<char, int>]

---
+++ Problem-1703B/Program.cs
@@ -0,0 +1,18 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int[] problems = new int[26];
+
+    int.TryParse(Console.ReadLine(), out int n);
+    string solvedProblems = Console.ReadLine();
+
+    for(int j = 0; j < n; j++)
+        problems[solvedProblems[j] - 65]++;
+
+    int balloons = 0;
+    for (int j = 0; j < 26; j++)
+        balloons += problems[j] > 0 ? problems[j] + 1 : 0;
+
+    Console.WriteLine(balloons);
+}
