16984218e85e65b985910e46674f0011c6247756
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-17 21:03:45 +0600

    A. Vus the Cossack and a Contest [Category: Min Finding]

---
+++ Problem-1186A/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] inputs = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+if (inputs[0] <= Math.Min(inputs[1], inputs[2]))
+    Console.WriteLine("YES");
+else Console.WriteLine("NO");
