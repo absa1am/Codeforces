abe90067d72f7ddd1d01e526b36e74f749110617
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-17 15:06:04 +0600

    A. C+= [Category: Loop]

---
+++ Problem-1368A/Program.cs
@@ -0,0 +1,20 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+
+    int a = int.Parse(parts[0]);
+    int b = int.Parse(parts[1]);
+    int n = int.Parse(parts[2]);
+
+    int count = 0;
+    while(a <= n && b <= n)
+    {
+        count++;
+        if (a < b) a += b;
+        else b += a;
+    }
+
+    Console.WriteLine(count);
+}
