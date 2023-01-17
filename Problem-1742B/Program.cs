69875eddb7d89e34a2bafd3f0017441c66277b9f
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-17 14:50:48 +0600

    B. Increasing [Category: Sorting, Duplicate Finding]

---
+++ Problem-1742B/Program.cs
@@ -0,0 +1,22 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    Array.Sort(numbers);
+
+    bool duplicateFound = false;
+    for(int j = 1; j < n; j++)
+    {
+        if (numbers[j] == numbers[j - 1])
+        {
+            duplicateFound = true;
+            break;
+        }
+    }
+
+    if (duplicateFound) Console.WriteLine("NO");
+    else Console.WriteLine("YES");
+}
