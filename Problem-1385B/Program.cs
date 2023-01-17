84737c2c43ac4c48bf5db1a8f364da0c83b32843
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-17 15:24:07 +0600

    B. Restore the Permutation by Merger [Category: HashSet<>]

---
+++ Problem-1385B/Program.cs
@@ -0,0 +1,14 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] mergedPermutation = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    HashSet<int> restoredPermutation = new HashSet<int>();
+
+    for(int j = 0; j < 2 * n; j++)
+        restoredPermutation.Add(mergedPermutation[j]);
+
+    Console.WriteLine(string.Join(' ', restoredPermutation.ToArray()));
+}
