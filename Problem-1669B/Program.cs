7ad501f9163b52d979f3b884ee8e4f10c2719d9f
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-18 16:11:37 +0600

    B. Triple [Category: Array or Dictionary<int, int>]

---
+++ Problem-1669B/Program.cs
@@ -0,0 +1,28 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int j = 0; j < t; j++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    string[] parts = Console.ReadLine().Split(' ');
+
+    int repeatedElem = -1;
+    Dictionary<int, int> map = new();
+
+    for(int i = 0; i < n; i++)
+    {
+        int x = int.Parse(parts[i]);
+
+        if(map.ContainsKey(x))
+        {
+            map[x]++;
+            if (map[x] == 3)
+            {
+                repeatedElem = x;
+                break;
+            }
+        }
+        else map.Add(x, 1);
+    }
+
+    Console.WriteLine(repeatedElem);
+}
