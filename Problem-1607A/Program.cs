d8596a99c8d1f3da0d89c0295a2098897d13ecf7
Abdus Salam <salaam.mbstu@gmail.com>
2023-01-18 15:47:38 +0600

    A. Linear Keyboard [Category: Array or Dictionary<char, int>]

---
+++ Problem-1607A/Program.cs
@@ -0,0 +1,17 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for(int i = 0; i < t; i++)
+{
+    string keyboard = Console.ReadLine();
+    string word = Console.ReadLine();
+
+    Dictionary<char, int> map = new();
+    for(int j = 0; j < keyboard.Length; j++)
+        map.Add(keyboard[j], j + 1);
+
+    int cost = 0;
+    for (int j = 1; j < word.Length; j++)
+        cost += Math.Abs(map[word[j]] - map[word[j - 1]]);
+
+    Console.WriteLine(cost);
+}
