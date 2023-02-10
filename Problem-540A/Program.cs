61b661c770efc3568a3a3ea16495c9be01da0c9a
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-02-11 03:54:32 +0600

    A. Combination Lock [Category: Math]

---
+++ Problem-540A/Program.cs
@@ -0,0 +1,13 @@
+﻿int.TryParse(Console.ReadLine(), out int n);
+string diskState = Console.ReadLine();
+string diskOpenPass = Console.ReadLine();
+
+int moves = 0;
+for(int i = 0; i < n; i++)
+{
+    int a = diskState[i] - '0';
+    int b = diskOpenPass[i] - '0';
+    moves += Math.Min(Math.Abs(a - b), 9 - Math.Max(a, b) + Math.Min(a, b) + 1);
+}
+
+Console.WriteLine(moves);
