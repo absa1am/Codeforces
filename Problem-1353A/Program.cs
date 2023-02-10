9e42239b57031a458c17deb7e294755c4caaeaee
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-02-11 05:43:19 +0600

    A. Most Unstable Array [Category: Math]

---
+++ Problem-1353A/Program.cs
@@ -0,0 +1,13 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for (int j = 0; j < t; j++)
+{
+    string[] parts = Console.ReadLine().Split(' ');
+
+    int n = int.Parse(parts[0]);
+    int m = int.Parse(parts[1]);
+
+    if (n == 1) Console.WriteLine("0");
+    else if (n == 2) Console.WriteLine(m);
+    else Console.WriteLine(2 * m);
+}
