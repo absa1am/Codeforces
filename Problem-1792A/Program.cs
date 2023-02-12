a2891bae8aaf164afc03799b05ba90884ffe92b6
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-02-12 12:12:01 +0600

    A. GamingForces [Category: Counting, Math]

---
+++ Problem-1792A/Program.cs
@@ -0,0 +1,15 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for (int j = 0; j < t; j++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+    int[] healths = Array.ConvertAll(Console.ReadLine().Split(' '), x => int.Parse(x));
+
+    int one = 0;
+    int operations = 0;
+    for (int i = 0; i < n; i++)
+        if (healths[i] == 1) one++;
+
+    operations = (one % 2) + (one / 2) + n - one;
+    Console.WriteLine(operations);
+}
