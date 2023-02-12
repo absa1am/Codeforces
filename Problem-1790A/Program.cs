ecaa1bed8bd8683a021075038e4b6b997b6922c4
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-02-12 12:14:28 +0600

    A. Polycarp and the Day of Pi [Category: String]

---
+++ Problem-1790A/Program.cs
@@ -0,0 +1,16 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for (int j = 0; j < t; j++)
+{
+    string digits = Console.ReadLine();
+    string pi = "314159265358979323846264338327";
+
+    int correct = 0;
+    for (int i = 0, k = 0; i < digits.Length; i++, k++)
+    {
+        if (digits[i] == pi[k]) correct++;
+        else break;
+    }
+
+    Console.WriteLine(correct);
+}
