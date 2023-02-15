dd9c27750d4012564a2221e0691cd3f9f2ad9bb8
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-02-15 15:44:16 +0600

    A. Night at the Museum [Category: clock and anti-clockwise rotation]

---
+++ Problem-731A/Program.cs
@@ -0,0 +1,14 @@
+﻿string s = Console.ReadLine();
+
+int move = 0;
+char start = 'a';
+for (int i = 0; i < s.Length; i++)
+{
+    int a = Math.Abs((s[i] - '0') - (start - '0'));
+    int b = 26 - Math.Abs((s[i] - '0') - (start - '0'));
+
+    start = s[i];
+    move += Math.Min(a, b);
+}
+
+Console.WriteLine(move);
