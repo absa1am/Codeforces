c58d1d4a45d762a870e8235dfbe774a4cd6bfbcf
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-02-15 16:06:32 +0600

    A. Special Permutation [Category: Loop, Odd]

---
+++ Problem-1454A/Program.cs
@@ -0,0 +1,21 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for (int j = 0; j < t; j++)
+{
+    int.TryParse(Console.ReadLine(), out int n);
+
+    int[] p = new int[n];
+
+    for (int i = n; i >= 1; i--)
+        p[n - i] = i;
+
+    if(n % 2 != 0)
+    {
+        int x = (n + 1) / 2;
+
+        p[x - 1] = 1;
+        p[n - 1] = x;
+    }
+
+    Console.WriteLine(string.Join(' ', p));
+}
