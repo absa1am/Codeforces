4bad6efacbd30922e165d3c921e03a63490cf747
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-05-12 16:32:05 +0600

    A. Love Story [Category: String comparison]

---
+++ Problem-1829A/Program.cs
@@ -0,0 +1,13 @@
+﻿int.TryParse(Console.ReadLine(), out int t);
+
+for (int i = 0; i < t; i++)
+{
+    string m = "codeforces";
+    string s = Console.ReadLine();
+
+    int answer = 0;
+    for (int j = 0; j < 10; j++)
+        if (s[j] != m[j]) answer++;
+
+    Console.WriteLine(answer);
+}
